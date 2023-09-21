80be04515bfdc229f959db2b59cd8f2eb907071c
isurojit <iamsurojit@hotmail.com>
2023-09-21 14:12:10 +0530

    Text To Speech Added

--- app.js
+++ app.js
@@ -3,4 +3,18 @@ const SpeechRecognition =
 
 const rec = new SpeechRecognition();
 
-//Parameter
+// Parameter
+rec.lang = "en-US";
+rec.continuous = true;
+
+rec.onresult = function (e) {
+  let result = "";
+  for (let i = e.resultIndex; i < e.results.length; i++) {
+    let script = e.results[i][0].transcript.toLowerCase().trim();
+    let scriptWithSpaces = script.split(" ").join(" ");
+    result += scriptWithSpaces + " "; // Append the result with a space
+  }
+  document.querySelector("#txtid").textContent += result; // Append the result
+};
+
+rec.start();
